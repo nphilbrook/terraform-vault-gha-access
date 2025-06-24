752f986fce173fe358ff4aea3840f2e3bcde6459
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-06-24 10:55:33 -0400

    bug fix and naming improvements

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 
-resource "vault_jwt_auth_backend_role" "int_gha_role" {
-  namespace         = var.vault_namespace_path
-  backend           = var.jwt_backend_path
+resource "vault_jwt_auth_backend_role" "gha_role" {
+  namespace = var.vault_namespace_path
+  backend   = var.jwt_backend_path
+
   role_name         = var.role_name
   bound_audiences   = ["https://github.com/${var.github_organization}"]
   bound_claims_type = "glob"
   bound_claims = {
-    sub                = join(",", [for repo in var.github_repositories : "repo:${var.github_organization}/${repo}"])
+    sub                = join(",", [for repo in var.github_repositories : "repo:${var.github_organization}/${repo}:ref:*"])
     workflow           = var.workflow
     runner_environment = "self-hosted"
   }
@@ -14,16 +15,16 @@ resource "vault_jwt_auth_backend_role" "int_gha_role" {
   role_type      = "jwt"
   token_ttl      = 300
   token_type     = "service"
-  token_policies = [vault_policy.int_gha_policy.name]
+  token_policies = [vault_policy.gha_policy.name]
 }
 
-resource "vault_policy" "int_gha_policy" {
+resource "vault_policy" "gha_policy" {
   namespace = var.vault_namespace_path
-  name      = "gha-policy"
-  policy    = data.vault_policy_document.int_gha_policy.hcl
+  name      = "${var.role_name}-policy"
+  policy    = data.vault_policy_document.gha_policy.hcl
 }
 
-data "vault_policy_document" "int_gha_policy" {
+data "vault_policy_document" "gha_policy" {
   rule {
     path         = "prod/kv/*"
     capabilities = ["read"]
